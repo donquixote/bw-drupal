136e07d2d113aac46a0d98bb6fedf59e5dc0dbe8
Andreas Hennings <andreas@dqxtech.net>
2012-05-06 16:15:30 +0200

    update admin_menu and author_pane

--- sites/all/modules/contrib/author_pane/author-pane-user-picture.tpl.php
+++ sites/all/modules/contrib/author_pane/author-pane-user-picture.tpl.php
@@ -1,31 +1,24 @@
 <?php
-
 /**
  * @file
  * Default theme implementation to present an picture configured for the
  * user's account.
  *
  * Available variables:
- * - $picture: Image set by the user or the site's default. Will use
+ * - $picture: Image set by the user or the site's default.
  * - $account: Array of account information. Potentially unsafe. Be sure to
  *   check_plain() before use.
- * - $imagecache_used: TRUE if imagecache was used to size the picture. This
- *   tells us if we want to link to the full sized image.
- *
- * Note that this intentionally does not link to the user page. If you want
- * it to link to the user page, change the link to:
- * <a href="/user/<?php print $account->uid; ?>"><?php print $picture; ?></a>
- *
+ * - $imagecache_used: TRUE if imagecache was used to size the picture.
+ * - $picture_link_profile: $picture with link to the user profile page if
+ *   the viewer has permission to view the user's profile page, otherwise FALSE.
+ * 
+ * Use $picture_link_profile instead of $picture if you want the picture to
+ * link to the account profile page.
  */
 ?>
 
 <?php if (!empty($picture)): ?>
   <div class="picture">
-    <?php if ($imagecache_used): ?>
-      <a href="<?php print file_create_url($account->picture->uri); ?>" rel="lightbox"><?php print $picture; ?></a>
-    <?php else: ?>
-      <?php print $picture; ?>
-    <?php endif; ?>
+    <?php print $picture; ?>
   </div>
 <?php endif; ?>
-
